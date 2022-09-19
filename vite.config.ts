bd79474a662a02f2a815675f314f62c6a1c86396
ame <25921047+zd613@users.noreply.github.com>
2022-09-19 10:28:58 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,5 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  base:'test-vue-publish-gh-pages'
+  base:'/test-vue-publish-gh-pages/'
 });
